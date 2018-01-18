d0b2856d336e91fe0e1e351d4d368ee83089c642
Gustavo Coronel <gcoronelc@gmail.com>
2018-01-17 22:27:30 -0500

    Clase 02

--- Clase02/AppPromedio/src/pe/egcc/apppromedio/AppPromedio.java
+++ Clase02/AppPromedio/src/pe/egcc/apppromedio/AppPromedio.java
@@ -1,9 +1,12 @@
 package pe.egcc.apppromedio;
 
+import pe.egcc.apppromedio.view.PromedioView;
+
 public class AppPromedio {
 
     public static void main(String[] args) {
       System.out.println("Hola todos, estoy con Java.");
+      PromedioView.main(args);
     }
     
 }
