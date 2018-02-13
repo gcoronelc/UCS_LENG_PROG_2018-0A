f3d22264d46a97b6ca6cfe109a928136fbc894c6
Gustavo Coronel <gcoronelc@gmail.com>
2018-02-12 19:42:28 -0500

    PROYECTOS

---
+++ PROY-AVA/GRUPO03/Grupo3/dos.php
@@ -0,0 +1,7 @@
+<HTML>
+<BODY>
+<?PHP
+   print ("La edad es: " . $_POST["edad"]);
+?>
+</BODY>
+</HTML>
---
+++ PROY-AVA/GRUPO03/Grupo3/formu1.php
@@ -0,0 +1,8 @@
+<HTML>
+<BODY>
+<FORM ACTION="dos.php" METHOD="POST">
+   Edad: <INPUT TYPE="text" NAME="edad">
+   <INPUT TYPE="submit" VALUE="aceptar">
+</FORM>
+</BODY>
+</HTML>
